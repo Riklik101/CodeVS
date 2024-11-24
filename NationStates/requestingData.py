dbc63964bb5dc90cd358ac6fc5566b0471416203
Eklavya Agrawal <iamanswer123472@gmail.com>
2024-11-24 14:46:20 -0600

    adding new info

--- NationStates/requestingData.py
+++ NationStates/requestingData.py
@@ -1,12 +1,17 @@
-#request data from URL, sort, and return with information
-
+import re
 import requests
 
 file_path = r'C:\Users\Eklavya\Documents\CodeVS\NationStates\data.txt'
 try:
     with open(file_path, 'r') as file:
         if (content := file.read()):
-            print(content)
+            print("Content Successfully Read")
+            
+            # Find all numbers that follow a hashtag
+            numbers = re.findall(r'#(\d+)', content)
+            
+#            # Print the extracted numbers
+ #           print(numbers)
 except FileNotFoundError:
     print(f'Error: The file at {file_path} was not found.')
 except Exception as e:
