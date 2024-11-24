6882fd84fdcb6e3216d1efe6c2ac1ed02a9e57a5
Eklavya Agrawal <iamanswer123472@gmail.com>
2024-11-23 23:56:01 -0600

    changed file management, new system

--- NationStates/requestingData.py
+++ NationStates/requestingData.py
@@ -1,6 +1,13 @@
 #request data from URL, sort, and return with information
 
 import requests
-file_path = 'data.txt'
-with open(file_path, 'r') as file:
-	print(file.read())
+
+file_path = r'C:\Users\Eklavya\Documents\CodeVS\NationStates\data.txt'
+try:
+    with open(file_path, 'r') as file:
+        if (content := file.read()):
+            print(content)
+except FileNotFoundError:
+    print(f'Error: The file at {file_path} was not found.')
+except Exception as e:
+    print(f'An error occurred: {e}')
